720abf57cbf73cd0558b63cc05730880da3ba397
Zoleda Davila <zoleda_davila@yahoo.com>
2023-05-05 14:44:30 -0400

    Created function structures for buttons in the menus

--- Portfolio2Project/Assets/Scripts/buttonFunctions.cs
+++ Portfolio2Project/Assets/Scripts/buttonFunctions.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Resume the game
+   public void resume()
+    {
+
+    }
+
+    //Restarts the level from the beginning
+    public void restart()
+    {
+
+    }
+
+    //Quits the game; doesn't work unless built
+    public void Quit()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //Respawn player from respawn location
+    public void respawnPLayer()
     {
-        
+
+    }
+
+    //Go to next level; doesn't work until next level is made
+    public void nextLevel()
+    {
+
     }
 }
