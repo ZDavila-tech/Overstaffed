44eb3c25c980ea0e9bb6604bb2a9265e2a8684cc
GogeSause <118745389+GogeSause@users.noreply.github.com>
2023-05-05 16:00:45 -0400

    enemyai derives from idamage now

--- Portfolio2Project/Assets/Scripts/EnemyAI.cs
+++ Portfolio2Project/Assets/Scripts/EnemyAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour, IDamage
 {
     [Header("----- Components -----")]
     [SerializeField] Renderer rModel;
