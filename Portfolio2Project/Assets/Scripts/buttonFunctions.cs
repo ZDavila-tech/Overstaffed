0d4bcc51030714232ca5207c68181ad382128ae3
Zoleda Davila <zoleda_davila@yahoo.com>
2023-05-05 14:54:17 -0400

    added code to button function methods and made UI a prefab

--- Portfolio2Project/Assets/Scripts/buttonFunctions.cs
+++ Portfolio2Project/Assets/Scripts/buttonFunctions.cs
@@ -8,27 +8,33 @@ public class buttonFunctions : MonoBehaviour
     //Resume the game
    public void resume()
     {
-
+        gameManager.instance.unPauseState();
     }
 
     //Restarts the level from the beginning
     public void restart()
     {
-
+        gameManager.instance.unPauseState();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     //Quits the game; doesn't work unless built
     public void Quit()
     {
-        
+        Application.Quit();
     }
 
     //Respawn player from respawn location
     public void respawnPLayer()
     {
-
+        gameManager.instance.unPauseState();
+        gameManager.instance.playerScript.spawnPlayer();
     }
 
+    public void playerHeal(int amount)
+    {
+        gameManager.instance.playerScript.playerHeal(amount);
+    }
     //Go to next level; doesn't work until next level is made
     public void nextLevel()
     {
