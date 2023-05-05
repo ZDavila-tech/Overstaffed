cd4af3ce44d885ed3f0aea1ec2437c00e69d1e66
SeotatopDehsam <jpcoots@student.fullsail.edu>
2023-05-05 14:16:38 -0400

    game manager work, variables set up.

---
+++ Portfolio2Project/Assets/Scripts/gameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [Header("------ Player Stuff -----")]
+
+    public GameObject player
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
