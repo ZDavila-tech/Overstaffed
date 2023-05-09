5d5c3a37a01c91b326a176d9bb8062e1e9759bee
Zoleda Davila <zoleda_davila@yahoo.com>
2023-05-09 17:28:00 -0400

    Update Hp at start to originalHP

--- Portfolio2Project/Assets/Scripts/PlayerController.cs
+++ Portfolio2Project/Assets/Scripts/PlayerController.cs
@@ -35,7 +35,7 @@ public class PlayerController : MonoBehaviour, IDamage
     // Start is called before the first frame update
     void Start()
     {
-
+        iHPOriginal = iHP;
     }
 
     // Update is called once per frame
