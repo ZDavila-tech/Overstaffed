28b8c886a651d27f4bf0d48e9ddda17b30f34a98
Victor Pinheiro Serra Pinto <VPinheiroSerraPinto@student.fullsail.edu>
2023-05-10 15:57:05 -0400

    Collectibles
    Healing and Poison orbs (poison orb is a hazard that works like the poison mushroom in Mario)

---
+++ Portfolio2Project/Assets/Scripts/Collectables.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    [SerializeField] int value;
+    /*enum CollectType 
+    { 
+      Health
+    }
+
+    [SerializeField] CollectType type;
+    */
+
+    private void OnTriggerEnter(Collider other)
+    {
+            if (other.CompareTag("Player"))
+            {
+                gameManager.instance.playerScript.TakeDamage(-value);
+                Destroy(gameObject);
+            }
+     }
+  }
