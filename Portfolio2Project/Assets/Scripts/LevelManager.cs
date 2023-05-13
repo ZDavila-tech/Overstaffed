e917788e18a85775c600bb7339b396d6bfb4edc6
DaisyOftheDay <theepicninjaott@gmail.com>
2023-05-13 18:47:56 -0400

    Door Animations, Raised Player POV

---
+++ Portfolio2Project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("-----Components-----")]
+    [SerializeField] Animator doorAnim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnim.SetBool("Open", true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnim.SetBool("Open", false);
+        }
+    }
+
+
+}
