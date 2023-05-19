a024e7c37d6d7894a73a64e6303284c7fea95b5e
DaisyOftheDay <theepicninjaott@gmail.com>
2023-05-19 17:29:43 -0400

    Enemy Spawn fix

--- Portfolio2Project/Assets/Scripts/Enemy/enemySpawn.cs
+++ Portfolio2Project/Assets/Scripts/Enemy/enemySpawn.cs
@@ -10,16 +10,20 @@ public class enemySpawn : MonoBehaviour
     [SerializeField] float spawnDelay;
 
     [Header("-----Enemies-----")]
-    [SerializeField] Transform[] enemies;
-    [SerializeField] int[] weights;
+    [SerializeField] private Transform[] enemies;
+    [Range(0,100)][SerializeField] private int[] weights;
     int totalWeight;
 
 
     LevelManager lm;
 
+
+    int arrayLen;
     // Start is called before the first frame update
     void Start()
     {
+        initializeLength();
+        sortArrays();
         lm = GetComponentInParent<LevelManager>();
         totalWeight = initializeEnemyWeight();
     }
@@ -51,7 +55,8 @@ public class enemySpawn : MonoBehaviour
 
     void spawn()
     {
-        Instantiate(weightedEnemySelection(),transform.position + spawnCoords(),Quaternion.identity,transform);
+        Transform toSpawn = weightedEnemySelection();
+        Instantiate(toSpawn,transform.position + spawnCoords(),Quaternion.identity,transform);
         lm.addCurr();
     }
 
@@ -62,16 +67,17 @@ public class enemySpawn : MonoBehaviour
 
     Transform weightedEnemySelection()
     {
-        int rand = Random.Range(0, totalWeight);
-        for (int i = 0; i < weights.Length; i++)
+        int rand = Random.Range(0, totalWeight -1);
+        for (int i = 0; i < arrayLen; i++)
         {
-            if (rand <= weights[i])
+            
+            if (rand <= totalWeight * (weights[i] / 100))
             {
                 return enemies[i];
             }
             
         }
-        return null;
+        return enemies[arrayLen - 1];
 
         
     }
@@ -80,4 +86,36 @@ public class enemySpawn : MonoBehaviour
     {
             Gizmos.DrawWireCube(new Vector3(transform.position.x,transform.position.y + 1,transform.position.z), new Vector3(spawnAreaX, 2, spawnAreaY));
     }
+
+    void sortArrays()
+    {
+        for (int i = 0; i < arrayLen - 1; i++)
+        {
+            for (int j = 0; j < arrayLen - i - 1; j++)
+                if (weights[j] > weights[j + 1])
+                {
+                    var temp = weights[j];
+                    var temp2 = enemies[j];
+                    weights[j] = weights[j + 1];
+                    enemies[j] = enemies[j + 1];
+                    weights[j + 1] = temp;
+                    enemies[j + 1] = temp2;
+                }
+        }
+    
+
+    }
+void initializeLength()
+{
+        arrayLen = 0;
+    foreach (int i in weights)
+        {
+            
+            arrayLen++;
+
+        }
 }
+
+}
+
+
