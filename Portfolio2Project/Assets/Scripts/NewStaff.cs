c426d700af501d451eca11ae5a75c8c2f8f7a82b
Victor Pinheiro Serra Pinto <VPinheiroSerraPinto@student.fullsail.edu>
2023-05-22 10:11:49 -0400

    Different Player Projectiles
    Made the prefabs

--- Portfolio2Project/Assets/Scripts/NewStaff.cs
+++ Portfolio2Project/Assets/Scripts/NewStaff.cs
@@ -12,7 +12,7 @@ public class NewStaff : MonoBehaviour
     [SerializeField] private ParticleSystem shootingSystem;
     [SerializeField] private Transform shootPos;
     [SerializeField] private ParticleSystem impactParticles;
-    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private List<TrailRenderer> trailRenderer;
     [SerializeField] private float delay;
     [SerializeField] private LayerMask mask;
 
@@ -97,7 +97,7 @@ public class NewStaff : MonoBehaviour
                 Vector3 direction = GetDirection();
                 if (Physics.Raycast(shootPos.position, direction, out RaycastHit hit, float.MaxValue, mask))
                 {
-                    TrailRenderer trail = Instantiate(trailRenderer, shootPos.position, Quaternion.identity);
+                    TrailRenderer trail = Instantiate(trailRenderer[(int)playerElement], shootPos.position, Quaternion.identity);
                     StartCoroutine(SpawnTrail(trail, hit));
                     lastShootTime = Time.time;
                 }
