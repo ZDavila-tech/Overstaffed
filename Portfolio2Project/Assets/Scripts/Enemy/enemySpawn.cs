79075d88b0693b78c855272663fc79426375f966
SeotatopDehsam <jpcoots@student.fullsail.edu>
2023-05-23 11:35:45 -0400

    enemySpawner

--- Portfolio2Project/Assets/Scripts/Enemy/enemySpawn.cs
+++ Portfolio2Project/Assets/Scripts/Enemy/enemySpawn.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawn : MonoBehaviour
+public class enemySpawn : MonoBehaviour
 {
     [Header("-----dimensions-----")]
     [SerializeField] float spawnAreaX;
