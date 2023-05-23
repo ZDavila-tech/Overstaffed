33173ff54395f482de8597d8cd24a3f515181a8e
Victor Pinheiro Serra Pinto <VPinheiroSerraPinto@student.fullsail.edu>
2023-05-23 13:55:05 -0400

    Music Restart and Fire Special Graphics

--- Portfolio2Project/Assets/Scripts/Orbit.cs
+++ Portfolio2Project/Assets/Scripts/Orbit.cs
@@ -8,6 +8,15 @@ public class Orbit : MonoBehaviour
     [SerializeField] float range;
     [SerializeField] float speed;
 
+    enum orbitType
+    {
+        Horizontal,
+        Diagonal,
+        Vertical
+    }
+
+    [SerializeField] orbitType type;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +26,17 @@ public class Orbit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (type)
+        {
+            case orbitType.Horizontal:
         transform.RotateAround(orbitpoint.transform.position, Vector3.up * range, speed * Time.deltaTime);
+                break;
+            case orbitType.Vertical:
+                transform.RotateAround(orbitpoint.transform.position, Vector3.forward * range, speed * Time.deltaTime);
+                break;
+            case orbitType.Diagonal:
+                transform.RotateAround(orbitpoint.transform.position, Vector3.right * range, speed * Time.deltaTime);
+                break;
+        }
     }
 }
