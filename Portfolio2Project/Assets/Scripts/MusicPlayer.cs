88b0eafcf1d512ca268fed054fffb40d43233503
Victor Pinheiro Serra Pinto <VPinheiroSerraPinto@student.fullsail.edu>
2023-05-23 11:28:41 -0400

    Reception BGM

--- Portfolio2Project/Assets/Scripts/MusicPlayer.cs
+++ Portfolio2Project/Assets/Scripts/MusicPlayer.cs
@@ -6,11 +6,13 @@ public class MusicPlayer : MonoBehaviour
 {
     [SerializeField] AudioSource aud;
     [SerializeField] List<AudioClip> bgms;
+    [SerializeField] float volume;
+    [SerializeField] int currSong;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        aud.PlayOneShot(bgms[currSong]);
     }
 
     // Update is called once per frame
