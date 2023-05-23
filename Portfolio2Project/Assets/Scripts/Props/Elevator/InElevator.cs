728df1f26f02e04f1bd1e8ad8209f8e86ca0c9dc
SeotatopDehsam <jpcoots@student.fullsail.edu>
2023-05-23 00:43:08 -0400

    Elevator now has lights and style

--- Portfolio2Project/Assets/Scripts/Props/Elevator/InElevator.cs
+++ Portfolio2Project/Assets/Scripts/Props/Elevator/InElevator.cs
@@ -8,6 +8,10 @@ public class InElevator : MonoBehaviour
     [SerializeField] GameObject door1;
     [SerializeField] GameObject door2;
 
+    [Header("----- Elecvator Light -----")]
+    [SerializeField] GameObject lightOn;
+    [SerializeField] GameObject lightOff;
+
     [Header("----- Set By Script -----")]
     public LevelManager levelManager;
 
@@ -22,13 +26,16 @@ public class InElevator : MonoBehaviour
         {
             door1.SetActive(false);
             door2.SetActive(false);
+            lightOn.SetActive(true);
+            lightOff.SetActive(false);
         }
         else
         {
             door1.SetActive(true);
             door2.SetActive(true);
-        }
-        
+            lightOn.SetActive(false);
+            lightOff.SetActive(true);
+        }        
     }
 
     private void OnTriggerEnter(Collider other)
