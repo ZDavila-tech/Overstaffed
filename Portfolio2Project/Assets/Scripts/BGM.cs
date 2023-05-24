67de1595f0f2d0df6bd23a7598d94051eb8eee51
Zoleda Davila <zoleda_davila@yahoo.com>
2023-05-24 14:00:53 -0400

    Edited BGM script and added volume slider in Main Menu

--- Portfolio2Project/Assets/Scripts/BGM.cs
+++ Portfolio2Project/Assets/Scripts/BGM.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BGM : MonoBehaviour
 {
     [SerializeField] AudioSource bgm;
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Toggle muteToggle;
 
-    void changeVolume(float volume)
+    private void Update()
     {
-        bgm.volume = volume;
+        changeVolume();
+    }
+
+    public void changeVolume()
+    {
+        bgm.volume = volumeSlider.value;
+        if(volumeSlider.value == 0)
+        {
+            muteToggle.isOn = true;
+        }
+        else
+        {
+            muteToggle.isOn = false;
+        }
     }
 }
