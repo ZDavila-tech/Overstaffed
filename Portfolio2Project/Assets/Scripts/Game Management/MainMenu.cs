2bbf534ec8ecf03b27f7bdc52d23a634eecb82ad
DaisyOftheDay <theepicninjaott@gmail.com>
2023-05-25 20:50:32 -0400

    Post-Build Push

--- Portfolio2Project/Assets/Scripts/Game Management/MainMenu.cs
+++ Portfolio2Project/Assets/Scripts/Game Management/MainMenu.cs
@@ -9,7 +9,8 @@ public class MainMenu : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
-        //fileManager.load();
+        fileManager.save();
+        fileManager.load();
     }
     public void PlayGame() //Takes player to character select scene
     {
--- Portfolio2Project/Assets/Scripts/Game Management/fileManager.cs
+++ Portfolio2Project/Assets/Scripts/Game Management/fileManager.cs
@@ -1,42 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
-using System.Text;
+
 
 public static class fileManager// : MonoBehaviour
 {
-    public static int masterVolume;
-    public static int effectVolume;
-    public static int musicVolume;
+    public static float masterVolume;
+    public static float effectVolume;
+    public static float musicVolume;
 
     public static bool invertY;
 
-    static DirectoryInfo dir = Directory.CreateDirectory("../saves");
-    
 
 
     public static void save()
     {
-        using (StreamWriter writer = new StreamWriter(dir.FullName))
-        {
-            writer.WriteLine(masterVolume + '\n');
-            writer.WriteLine(effectVolume + "\n");
-            writer.WriteLine(musicVolume + "\n");
-            writer.WriteLine(invertY);
-            writer.Close();
-        }
+        PlayerPrefs.SetFloat("MV", masterVolume);
+        PlayerPrefs.SetFloat("EV", effectVolume);
+        PlayerPrefs.SetFloat("MuV", musicVolume);
+        PlayerPrefs.SetString("inv", invertY.ToString());
     }
 
     public static void load()
     {
-        using (StreamReader reader = new StreamReader(dir.FullName))
-        {
-            masterVolume = int.Parse(reader.ReadLine());
-            effectVolume = int.Parse(reader.ReadLine());
-            musicVolume = int.Parse(reader.ReadLine());
-            invertY= bool.Parse(reader.ReadLine());
-        }
+        masterVolume = PlayerPrefs.GetFloat("MV");
+        effectVolume = PlayerPrefs.GetFloat("EV");
+        musicVolume = PlayerPrefs.GetFloat("MuV");
+        invertY = bool.Parse( PlayerPrefs.GetString("inv"));
+
     }
-    
+
 }
