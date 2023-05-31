124f105a4d03e5312dfaeec31d647c6124ac6afe
SeotatopDehsam <jpcoots@student.fullsail.edu>
2023-05-31 15:50:18 -0400

    Organization

--- Portfolio2Project/Assets/Scripts/DontDOL.cs
+++ Portfolio2Project/Assets/Scripts/DontDOL.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI : MonoBehaviour
+public class DontDOL : MonoBehaviour
 {
     private void Awake()
     {
