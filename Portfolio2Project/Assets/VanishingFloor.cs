59e3da539f420e32a88437cc0d4543c1dfdf6229
DaisyOftheDay <theepicninjaott@gmail.com>
2023-06-03 22:31:54 -0400

    Completed Vanishing Platform

---
+++ Portfolio2Project/Assets/VanishingFloor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishingFloor : MonoBehaviour
+{
+    [SerializeField] float vanishDelay;
+    [SerializeField] float appearDelay;
+    [SerializeField] Transform particle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(vanish());
+    }
+
+    IEnumerator vanish()
+    {
+        yield return new WaitForSeconds(vanishDelay);
+        ParticleSystem particleComp = Instantiate(particle,transform.position,transform.rotation).GetComponent<ParticleSystem>();
+        var shape = particleComp.shape;
+        shape.scale = transform.localScale;
+        this.gameObject.SetActive(false);
+    }
+
+    IEnumerator appear()
+    {
+        yield return new WaitForSeconds(appearDelay);
+        this.gameObject.SetActive(true);
+    }
+}
