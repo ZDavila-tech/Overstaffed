1ef34dc909bc6365286df2c7de3d3f7a2c1d0652
Xavier <118745930+xavierwilliams2004@users.noreply.github.com>
2023-06-05 16:00:01 -0400

    Inventory and items added

---
+++ Portfolio2Project/Assets/Scripts/Items/HealingPotion.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingPotion : MonoBehaviour
+{
+    [SerializeField] PlayerController controller;
+    [SerializeField] public int healAmount;
+    bool itemUsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<MeshRenderer>().enabled != true)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            controller.TakeDamage(healAmount * -1);
+            controller.items.Remove(controller.items[controller.itemSelected]);
+            itemUsed = true;
+        }
+        
+    }
+}
