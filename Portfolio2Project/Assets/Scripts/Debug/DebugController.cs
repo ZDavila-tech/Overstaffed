da4baa8005f212270410f53b1e47802f43499a47
Xavier <118745930+xavierwilliams2004@users.noreply.github.com>
2023-06-12 11:50:08 -0400

    Added commands to skip levels

--- Portfolio2Project/Assets/Scripts/Debug/DebugController.cs
+++ Portfolio2Project/Assets/Scripts/Debug/DebugController.cs
@@ -14,6 +14,9 @@ public class DebugController : MonoBehaviour
 
     public static DebugCommand GIVE_EXP;
     public static DebugCommand GOD_MODE;
+    public static DebugCommand SKIP_LEVEL;
+    public static DebugCommand SKIP_TO_BOSS;
+    public static DebugCommand SKIP_TUTORIAL;
 
     public List<object> commandList;
 
@@ -22,11 +25,18 @@ public class DebugController : MonoBehaviour
     {
         GIVE_EXP = new DebugCommand("give_exp~", "Gives 9999 EXP", "give_exp~", () => { player.playerStats.Exp += 9999; });
         GOD_MODE = new DebugCommand("god_mode~", "Prevents you from taking damage", "god_mode~", () => { player.godMode = !player.godMode; });
-
+        SKIP_LEVEL = new DebugCommand("skip_level~", "Sets the current level to the next one", "skip_level~", () => { gameManager.instance.levelManager.currentLevel += 1; });
+        SKIP_TO_BOSS = new DebugCommand("skip_to_boss~", "Skips to the boss level", "skip_to_boss~", () => { gameManager.instance.levelManager.currentLevel = 20; });
+        SKIP_TUTORIAL = new DebugCommand("skip_tutorial~", "Skips to the end of the tutorial", "skip_tutorial~", () => { gameManager.instance.levelManager.currentLevel = 6; });
 
         commandList = new List<object>
         {
-            GIVE_EXP, GOD_MODE, 
+            GIVE_EXP, 
+            GOD_MODE, 
+            SKIP_LEVEL,
+            SKIP_TO_BOSS,
+            SKIP_TUTORIAL,
+
         };
     }
 
@@ -38,9 +48,12 @@ public class DebugController : MonoBehaviour
 
     public void OnToggleDebug()
     {
+        
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             showConsole = !showConsole;
+
+            
         }
     }
 
@@ -75,6 +88,8 @@ public class DebugController : MonoBehaviour
     
     public void HandleInput() //Checks to see if you entered a valid command and executes it
     {
+        string[] properties = input.Split(' ');
+
         for (int i = 0; i < commandList.Count; i++)
         {
             DebugCommandBase commandBase = commandList[i] as DebugCommandBase;
@@ -84,8 +99,12 @@ public class DebugController : MonoBehaviour
                 (commandList[i] as DebugCommand).InvokeCommand();
                 if (commandList[i] as DebugCommand != null)
                 {
-                    Debug.Log("Code Hit");
                     (commandList[i] as DebugCommand).InvokeCommand();
+                }
+                else if(commandList[i] as DebugCommand<int> != null)
+                {
+                    Debug.Log(properties[1]);
+                    (commandList[i] as DebugCommand<int>).InvokeCommand(int.Parse(properties[1]));
                 }
             }
         }
